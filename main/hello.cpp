69bde304bf166706899cd1713a9eb0e9e0af00f8
Akash Anand <akashanandscorpio@gmail.com>
2025-03-13 13:26:20 +0530

    Made a change

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this\n";
   cout << "My SRN is PES1UG22CS056\n";
+  cout << "I made a change\n";
   return 0;
 }
